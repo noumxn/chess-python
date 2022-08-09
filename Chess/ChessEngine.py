95f7916b1207bcdbd2e2f6c667145642356f44d1
Nouman Syed <syednouman2114@gmail.com>
2022-08-09 23:51:23 +0530

    chess grid done with pieces set to initial positions

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -3,6 +3,10 @@
 
 class GameState:
     def __init__(self):
+        # board is 8x8 2D list, each element is represented by two charecters
+        # 'w' or 'b' represents 'white' or 'black'
+        # 'R', 'N', 'B', 'Q', 'K' and 'p' represent 'Rook', 'Knight', 'Bishop', 'Queen', 'King' and 'pawn' respectively
+        # "--" represents empty space with no piece
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bK", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -13,3 +17,6 @@ class GameState:
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wK", "wR"],
         ]
+
+        self.whiteToMove = True
+        self.moveLog = []
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -1,1 +1,67 @@
 # Handles user input and displaying current GameState object
+
+import pygame as p
+
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+# Initialize a global dictionary for images. Might give user option to their own chess set as the ones used in this
+# project are not very high resolution
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(
+            p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE)
+        )
+
+
+# Main driver for handling user input and updates graphics
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+# Draws squares and pieces on the board and responsible for all graphics on the board
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draws squares on the board
+    drawPieces(screen, gs.board)  # draws pieces on top of squares
+
+# Draw squares on the board, top-left square always light
+def drawBoard(screen):
+    colors = [p.Color("light grey"), p.Color("dark grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+main()
