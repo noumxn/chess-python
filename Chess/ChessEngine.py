986037fc922a30b0bcaf2768fe6aa05c7d279405
Nouman Syed <syednouman2114@gmail.com>
2022-08-10 02:02:37 +0530

    done with moving the pieces, facing corruption issues with player_clicks list

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,4 +1,5 @@
-# Stores all info on current state of the game. Also determines what moves are valid in current position. Keeps move log.
+# Stores all info on current state of the game. Also determines what moves are valid in current position.
+# Keeps move log.
 
 
 class GameState:
@@ -20,3 +21,32 @@ class GameState:
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+
+class Move:
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 7, "b": 6, "c": 5, "d": 4,
+                   "e": 3, "f": 2, "g": 1, "h": 0}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __int__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -32,10 +32,29 @@ def main():
     gs = ChessEngine.GameState()
     loadImages()
     running = True
+    sq_selected = ()
+    player_clicks = []
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # mouse click location
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sq_selected == (row, col):  # User clicks source and destination is the same/ illegal move
+                    sq_selected = ()  # Unselecting stored clicks and positions made by
+                    player_clicks = []  # player during illegal move
+                else:
+                    sq_selected = (row, col)
+                    player_clicks.append(sq_selected)
+                if len(player_clicks) == 2:
+                    move = ChessEngine.Move(player_clicks[0], player_clicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sq_selected = ()
+                    player_clicks = []
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -46,13 +65,14 @@ def drawGameState(screen, gs):
     drawBoard(screen)  # draws squares on the board
     drawPieces(screen, gs.board)  # draws pieces on top of squares
 
+
 # Draw squares on the board, top-left square always light
 def drawBoard(screen):
     colors = [p.Color("light grey"), p.Color("dark grey")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            color = colors[((r+c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 def drawPieces(screen, board):
@@ -60,8 +80,8 @@ def drawPieces(screen, board):
         for c in range(DIMENSION):
             piece = board[r][c]
             if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-
-main()
+if __name__ == "__main__":
+    main()
